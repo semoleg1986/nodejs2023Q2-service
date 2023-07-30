8338da9572f048cef24d4583eaa02a770d62d615
Oleg Semenov <semoleg1986@gmail.com>
2023-07-30 17:28:20 +0300

    fix: impl. Users DOC

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
-  @ApiProperty({ required: true, description: 'User login' })
+  @ApiProperty({ required: true, example: 'test', description: 'User login' })
   login: string;
-  @ApiProperty({ required: true, description: 'User password' })
+  @ApiProperty({
+    required: true,
+    example: 'example',
+    description: 'User password',
+  })
   password: string;
 }
