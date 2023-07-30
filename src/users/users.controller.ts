77e9584a1ffd0a6f68cdb4264f33bff71ceb8618
Oleg Semenov <semoleg1986@gmail.com>
2023-07-30 14:44:12 +0300

    feat: impl. Users DOC

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,36 +17,63 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import {
+  ApiBadRequestResponse,
   ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
+  ApiResponse,
   ApiTags,
+  ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
+import { UnauthorizedError } from 'src/utils/UnauthorizedError';
 
 @ApiTags('Users')
 @Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  @ApiOperation({ summary: 'Create a new user' })
-  @ApiCreatedResponse({
-    description: 'The user has been created.',
-    type: CreateUserDto,
-  })
+
   @Post()
-  @ApiOkResponse({ type: [User] })
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Create user', description: 'Creates a new user' })
+  @ApiCreatedResponse({ description: 'The user has been created.', type: User })
+  @ApiBadRequestResponse({
+    description: 'Bad request. body does not contain required fields',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   create(@Body() createUserDto: CreateUserDto): User {
     return plainToClass(User, this.usersService.create(createUserDto));
   }
-  @ApiOperation({ summary: 'Get all users' })
-  @ApiOkResponse({ description: 'Got all users.', type: [User] })
+
   @Get()
+  @ApiOperation({ summary: 'Get all users', description: 'Gets all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful operation',
+    type: [User],
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    type: UnauthorizedError,
+  })
   findAll() {
     return this.usersService.findAll();
   }
-  @ApiOperation({ summary: 'Get user by id' })
-  @ApiOkResponse({ description: 'Got user by id.', type: [User] })
+
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get single user by id',
+    description: 'Get single user by id',
+  })
+  @ApiOkResponse({ description: 'Successful operation', type: User })
+  @ApiBadRequestResponse({
+    description: 'Bad request. userId is invalid (not uuid)',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'User not found' })
   findOne(@Param('id') id: string) {
     if (!this.usersService.isValidUserId(id)) {
       throw new BadRequestException('Invalid userId');
@@ -57,19 +84,35 @@ export class UsersController {
     }
     return user;
   }
-  @ApiOperation({ summary: "Update user's password" })
-  @ApiOkResponse({ description: "User's password changed", type: [User] })
+
   @Put(':id')
+  @ApiOperation({
+    summary: "Update a user's password",
+    description: "Updates a user's password by ID",
+  })
+  @ApiOkResponse({ description: 'The user has been updated.', type: User })
+  @ApiBadRequestResponse({
+    description: 'Bad request. userId is invalid (not uuid)',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiForbiddenResponse({ description: 'oldPassword is wrong' })
+  @ApiNotFoundResponse({ description: 'User not found' })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     if (!this.usersService.isValidUserId(id)) {
       throw new BadRequestException('Invalid userId');
     }
     return plainToClass(User, this.usersService.update(id, updateUserDto));
   }
-  @ApiOperation({ summary: 'Delete user password' })
-  @ApiOkResponse({ description: 'User has been delete' })
+
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete user', description: 'Deletes user by ID' })
+  @ApiNoContentResponse({ description: 'The user has been deleted' })
+  @ApiBadRequestResponse({
+    description: 'Bad request. userId is invalid (not uuid)',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'User not found' })
   remove(@Param('id') id: string) {
     if (!this.usersService.isValidUserId(id)) {
       throw new BadRequestException('Invalid userId');
