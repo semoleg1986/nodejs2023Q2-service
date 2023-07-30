3d817ca6e467dd4b011c65c78c4bd6afac398346
Oleg Semenov <semoleg1986@gmail.com>
2023-07-30 17:46:17 +0300

    feat:impl. Artist DOC

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -15,24 +15,54 @@ import { ArtistsService } from './artists.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 import { Artist } from './entities/artist.entity';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('Artist')
 @Controller('artist')
 export class ArtistsController {
   constructor(private readonly artistsService: ArtistsService) {}
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Add new artist', description: 'Add new artist' })
+  @ApiCreatedResponse({ description: 'Successful operation', type: Artist })
+  @ApiBadRequestResponse({
+    description: 'Bad request. body does not contain required fields',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   create(@Body() createArtistDto: CreateArtistDto): Artist {
     const newArtist = this.artistsService.create(createArtistDto);
     return newArtist;
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all artists', description: 'Gets all artists' })
+  @ApiOkResponse({ description: 'Successful operation', type: [Artist] })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   findAll() {
     return this.artistsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get single artist by id',
+    description: 'Get single artist by id',
+  })
+  @ApiOkResponse({ description: 'Successful operation', type: Artist })
+  @ApiNotFoundResponse({ description: 'Artist was not found' })
+  @ApiBadRequestResponse({
+    description: 'Bad request. artistId is invalid (not uuid)',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   findOne(@Param('id') id: string) {
     if (!this.artistsService.isValidArtistId(id)) {
       throw new BadRequestException('Invalid artistId');
@@ -46,6 +76,16 @@ export class ArtistsController {
 
   @Put(':id')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Update artist information',
+    description: 'Update artist information by UUID',
+  })
+  @ApiOkResponse({ description: 'The artist has been updated.', type: Artist })
+  @ApiNotFoundResponse({ description: 'Artist was not found' })
+  @ApiBadRequestResponse({
+    description: 'Bad request. artistId is invalid (not uuid)',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   update(
     @Param('id') id: string,
     @Body() updateArtistDto: UpdateArtistDto,
@@ -57,6 +97,16 @@ export class ArtistsController {
   }
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete artist',
+    description: 'Delete artist from library',
+  })
+  @ApiNoContentResponse({ description: 'Deleted successfully' })
+  @ApiNotFoundResponse({ description: 'Artist was not found' })
+  @ApiBadRequestResponse({
+    description: 'Bad request. artistId is invalid (not uuid)',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   remove(@Param('id') id: string) {
     if (!this.artistsService.isValidArtistId(id)) {
       throw new BadRequestException('Invalid artistId');
--- src/artists/dto/create-artist.dto.ts
+++ src/artists/dto/create-artist.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsBoolean, IsNotEmpty } from 'class-validator';
 
 export class CreateArtistDto {
+  @ApiProperty({ required: true, example: 'Freddie Mercury' })
   @IsNotEmpty()
   @IsString()
   name: string;
+  @ApiProperty({ required: false, example: false })
   @IsNotEmpty()
   @IsBoolean()
   grammy: boolean;
