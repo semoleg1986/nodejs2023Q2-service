ce39b90c692febe41db8bf0643ab91d39e35d5ad
Oleg Semenov <semoleg1986@gmail.com>
2023-07-30 17:01:10 +0300

    feat: impl. Tracks DOC

--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -14,24 +14,68 @@ import {
 import { TracksService } from './tracks.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { Track } from './entities/track.entity';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('Track')
 @Controller('track')
 export class TracksController {
   constructor(private readonly tracksService: TracksService) {}
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    summary: 'Add new track',
+    description: 'Add new track information',
+  })
+  @ApiCreatedResponse({ description: 'Successful operation', type: Track })
+  @ApiBadRequestResponse({
+    description: 'Bad request. Body does not contain required fields',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   create(@Body() createTrackDto: CreateTrackDto) {
     const newTrack = this.tracksService.create(createTrackDto);
     return newTrack;
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Get tracks list',
+    description: 'Gets all library tracks list',
+  })
+  @ApiOkResponse({ description: 'Successful operation', type: [Track] })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   findAll() {
     return this.tracksService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get single track by id',
+    description: 'Get single track by id',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'The id of the track',
+    type: 'string',
+    format: 'uuid',
+  })
+  @ApiOkResponse({ description: 'Successful operation', type: Track })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Track was not found' })
+  @ApiBadRequestResponse({
+    description: 'Bad request. trackId is invalid (not uuid)',
+  })
   findOne(@Param('id') id: string) {
     if (!this.tracksService.isValidTrackId(id)) {
       throw new BadRequestException('Invalid trackId');
@@ -45,6 +89,16 @@ export class TracksController {
 
   @Put(':id')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Update track information',
+    description: 'Update library track information by UUID',
+  })
+  @ApiOkResponse({ description: 'The track has been updated.', type: Track })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Track was not found' })
+  @ApiBadRequestResponse({
+    description: 'Bad request. trackId is invalid (not uuid)',
+  })
   update(@Param('id') id: string, @Body() updateTrackDto: UpdateTrackDto) {
     if (!this.tracksService.isValidTrackId(id)) {
       throw new BadRequestException('Invalid trackId');
@@ -54,6 +108,16 @@ export class TracksController {
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete track',
+    description: 'Delete track from library',
+  })
+  @ApiNoContentResponse({ description: 'Deleted successfully' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Track was not found' })
+  @ApiBadRequestResponse({
+    description: 'Bad request. trackId is invalid (not uuid)',
+  })
   remove(@Param('id') id: string) {
     if (!this.tracksService.isValidTrackId(id)) {
       throw new BadRequestException('Invalid trackId');
