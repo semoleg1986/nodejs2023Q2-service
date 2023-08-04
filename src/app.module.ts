750e2e3f6efdb65045c149ecbce24bb5c634ff1b
semoleg1986 <semoleg1986@gmail.com>
2023-08-04 16:17:29 +0300

    feat: to ready to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { TracksModule } from './tracks/tracks.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
 import { FavoritesModule } from './favorites/favorites.module';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   url: process.env.DATABASE_URL,
+    //   autoLoadEntities: true,
+    //   synchronize: true
+    // }),
     UsersModule,
     ConfigModule.forRoot(),
     TracksModule,
