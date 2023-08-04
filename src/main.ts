151f2dbb53654f6683bd34ec7b95e6ce790218b0
semoleg1986 <semoleg1986@gmail.com>
2023-08-04 16:13:36 +0300

    refactor: create new doc

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as YAML from 'yamljs';
 import * as path from 'path';
-import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const apiSpecPath = path.join('doc', 'api.yaml');
-  const apiSpec = YAML.load(apiSpecPath);
-  const document = SwaggerModule.createDocument(app, apiSpec);
+  // const apiSpecPath = path.join(__dirname, 'doc', 'api.yaml');
+  // const apiSpec = YAML.load(apiSpecPath);
+  const config = new DocumentBuilder()
+  .setTitle('Home Library Service')
+  .setDescription('Home music library service')
+  .setVersion('1.0.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
   await app.listen(port);
 }
