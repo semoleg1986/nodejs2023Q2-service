70047da2192311b6963a233ad29867f3fc4b0621
semoleg1986 <semoleg1986@gmail.com>
2023-08-05 16:57:38 +0300

    feat: impl. TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,16 @@ import { TracksModule } from './tracks/tracks.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
 import { FavoritesModule } from './favorites/favorites.module';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   url: process.env.DATABASE_URL,
-    //   autoLoadEntities: true,
-    //   synchronize: true
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
     UsersModule,
     ConfigModule.forRoot(),
     TracksModule,
