9deec7005acb39e5ff2ed6fb490d808fee38cb0d
semoleg1986 <semoleg1986@gmail.com>
2023-08-19 21:28:04 +0300

    feat: impl. Logger

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -8,13 +8,14 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const { method, originalUrl, query, body } = req;
 
-    this.logger.log(`Incoming request: ${method} ${originalUrl}`);
-    this.logger.log(`Query parameters: ${JSON.stringify(query)}`);
-    this.logger.log(`Request body: ${JSON.stringify(body)}`);
-
     res.on('finish', () => {
+      // const contentLength = res.get('content-length');
       const { statusCode } = res;
-      this.logger.log(`Outgoing response: ${statusCode}`);
+      this.logger.log(
+        `${method} ${originalUrl}: Query parameters: ${JSON.stringify(
+          query,
+        )}, Body: ${JSON.stringify(body)}, ${statusCode}.)}`,
+      );
     });
 
     next();
